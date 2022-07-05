2ed8325c979e38c7d117a2b54bc82eb1f85db43b
caperaven <caperaven@gmail.com>
2022-07-05 10:30:29 +0200

    add css grid editor HTML file

---
+++ app/css-grid/css-grid.js
@@ -0,0 +1,7 @@
+import "./../../components/css-grid-editor/css-grid-editor.js"
+
+export default class CssGridViewModel extends crsbinding.classes.ViewBase {
+    async connectedCallback() {
+        await super.connectedCallback();
+    }
+}
