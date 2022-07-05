1320465daa6c2b6f76f79fef07190badcb1075da
caperaven <caperaven@gmail.com>
2022-07-05 09:24:08 +0200

    add css grid editor HTML file

---
+++ components/css-grid-editor/css-grid-editor.js
@@ -0,0 +1,15 @@
+export class CssGridEditor extends HTMLElement {
+    async connectedCallback() {
+        this.innerHTML = await fetch(import.meta.url.replace(".js", ".html")).then(result => result.text());
+
+        requestAnimationFrame(() => {
+            // initialize code
+        })
+    }
+
+    async disconnectedCallback() {
+
+    }
+}
+
+customElements.define("css-grid-editor", CssGridEditor);
