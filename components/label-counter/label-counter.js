addfcdda96e7d0e01b28b80843e79bbc33319b6b
6Botter7 <7williamsandre7@gmail.com>
2022-07-11 23:38:08 +0200

    Styling added with clickEvent Attempts
    Some Styling done on the Css-Grid-Editor and Label Counter Component.
    
    Attempted the clickEvent.
    Values added , but as String Value and updated value not rendering on screen

--- components/label-counter/label-counter.js
+++ components/label-counter/label-counter.js
@@ -1,15 +1,40 @@
 export class LabelCounter extends HTMLElement{
     async connectedCallback(){
         this.innerHTML = await fetch(import.meta.url.replace(".js", ".html")).then(result => result.text());
+        this.dataset.value = 0;
+        this.plus = this.querySelector(".increase");
+        this.minus = this.querySelector(".decrease");
+
+        //add click event
+        this.clickHandler = this.clicked.bind(this);  
+        this.addEventListener("click", this.clickHandler);
 
         requestAnimationFrame(() => {
             // initialize code
+            this.querySelector("[data-id='label']").textContent = this.dataset.label;
         })
         // alert("Connected");
     }
 
     async disconnectedCallback(){
+        await this.removeEventListener("click",this.clickHandler);
+        this.clickHandler = null;
+    }
 
+    async clicked(event){
+        if (event.target.dataset.id == "decrease"){
+            if(this.dataset.value > 1){
+                this.dataset.value --;
+        }
+        }
+        else if (event.target.dataset.id == "increase"){
+            this.dataset.value += 1;
+        }
+
+    }
+
+    renderCounter() {
+        document.getElementById("value").innerHTML = this.counter
     }
 }
 
