8182401c0d2471b52b541db79ab654f4d76490fc
6Botter7 <95472607+6Botter7@users.noreply.github.com>
2022-07-28 11:37:10 +0200

    Revert "Feature aw cssgrid "

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -1,5 +1,4 @@
 import "./../../components/label-counter/label-counter.js"
-import "./../../components/hello-world/hello-world.js"
 
 export default class Welcome extends crsbinding.classes.ViewBase {
     async connectedCallback() {
--- components/hello-world/hello-world.js
+++ components/hello-world/hello-world.js
@@ -1,7 +1,13 @@
 export class HelloWorld extends HTMLElement {
-    async connectedCallback(){}
+    async connectedCallback(){
+        this.innerHTML = await fetch(import.meta.url.replace(".js", ".html")).then(result => result.text());
 
-    async disconnectedCallback(){}
-        
+
+    }
+
+    async disconnectedCallback(){
+
+    }
 }
 
+customElements.define("hello-world", HelloWorld);
