06ed79b293f610ca80a13d46b58b5d5f88ef5ed2
6Botter7 <95472607+6Botter7@users.noreply.github.com>
2022-08-24 08:38:03 +0200

    Revert "Designer-repo shifted"

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -1,5 +1,3 @@
-import "./../../components/designer-repo/designer-repo.js";
-
 export default class Welcome extends crsbinding.classes.ViewBase {
     async connectedCallback() {
         await super.connectedCallback();
