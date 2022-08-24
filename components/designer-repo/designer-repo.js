ee667570e196286950150509d3cfc72e17faae3a
caperaven <caperaven@gmail.com>
2022-08-24 08:55:59 +0200

    review comments

--- components/designer-repo/designer-repo.js
+++ components/designer-repo/designer-repo.js
@@ -1,7 +1,15 @@
+/**
+ * JHR: Code order is wrong.
+ * First properties
+ * Then Lifecycle events (constructor, connectedCallback, disconnectedCallback)
+ * Then Methods
+ */
+
 export class DesignerRepo extends crsbinding.classes.BindableElement{
     get html() { 
         return import.meta.url.replace(".js", ".html");
-        
+
+        // JHR: this does not belong here, move to connected callback
         this.loadHTML();
     }
 
@@ -9,12 +17,14 @@ export class DesignerRepo extends crsbinding.classes.BindableElement{
         this.setProperty ("displayStatus", "grid");
     }
 
+    // JHR Make this async because fetch is async
     loadHTML () {
         const status = this.getProperty("displayStatus");
         const file = `/templates/designer-repo/${this.dataset.repo}-${status}.html`;
-    }
 
-       
+        // 1. use fetch api to fetch the hml
+        // 2. append the html to the ul
+    }
 
     async connectedCallback() {
         await super.connectedCallback();
