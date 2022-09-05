3638355f7c586b6f016ba7fb82b64bc20a0df667
6Botter7 <95472607+6Botter7@users.noreply.github.com>
2022-09-05 23:32:20 +0200

    Css and Html updated
    Added new styling to css
    Added SVG icon as Buttons
    Added SVGs for grid display
    Corrected Naming for elements and Css attributes.
    
    Few Problems however, to be discussed on 06/09/2022

--- components/designer-repo/designer-repo.js
+++ components/designer-repo/designer-repo.js
@@ -13,13 +13,12 @@ export class DesignerRepo extends crsbinding.classes.BindableElement{
 
     async connectedCallback() {
         await super.connectedCallback();
+        this.display = this.querySelector("#display")
         this.loadHTML();
-        this.display = this.querySelector("#display");
-
     }
 
     async disconnectedCallback () {
-
+        this.display = null;
         await super.disconnectedCallback();
     }
 
@@ -27,7 +26,6 @@ export class DesignerRepo extends crsbinding.classes.BindableElement{
         this.setProperty ("displayStatus", "grid");
     }
 
-    // JHR Make this async because fetch is async
     async loadHTML () {
         const status = this.getProperty("displayStatus");
         const file = `/templates/designer-repo/${this.dataset.repo}-${status}.html`;
@@ -36,6 +34,8 @@ export class DesignerRepo extends crsbinding.classes.BindableElement{
         console.log(var1);
 
         this.display.innerHTML = await fetch(file).then(result => result.text());
+
+        // this.container.innerHTML = await fetch(file).then(result => result.text());
         
         // 1. use fetch api to fetch the html
         // 2. append the html to the ul
