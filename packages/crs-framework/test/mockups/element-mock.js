c827802d6f2a4dd8e3ba862ceb29cde91a51dca3
caperaven <caperaven@gmail.com>
2022-09-29 22:44:53 +0200

    refactor testing and packages

--- packages/crs-framework/test/mockups/element-mock.js
+++ packages/crs-framework/test/mockups/element-mock.js
@@ -15,6 +15,7 @@ export class ElementMock {
 
 export function mockElement(instance, tag, id) {
     instance.__events = [];
+    instance.queryResults = {};
 
     instance.nodeName = (tag || "div").toUpperCase();
     instance.id = id;
@@ -85,6 +86,10 @@ function removeAttribute (attr) {
 }
 
 function querySelector(selector) {
+    if (this.queryResults[selector] != null) {
+        return this.queryResults[selector];
+    }
+
     const callback = createQueryFunction(selector);
     return find(this, callback);
 }
@@ -104,7 +109,6 @@ function appendChild(element) {
     this.children.push(element);
     element.parentElement = this;
     return element;
-
 }
 
 function removeChild(child) {
--- tests/components/label-counter.test.js
+++ tests/components/label-counter.test.js
@@ -2,6 +2,7 @@ import { beforeAll, afterAll, afterEach, beforeEach, describe, it} from "https:/
 import { assertEquals, assertExists, assert } from "https://deno.land/std@0.149.0/testing/asserts.ts";
 import {init} from "./../../packages/crs-framework/test/mockups/init.js";
 import {createMockChildren} from "./../../packages/crs-framework/test/mockups/child-mock-factory.js";
+import {ElementMock} from "../../packages/crs-framework/test/mockups/element-mock.js";
 
 await init();
 
@@ -14,7 +15,8 @@ describe("label-counter tests", () => {
 
     beforeEach(async () => {
         instance = document.createElement("label-counter");
-        instance.querySelector = (query) => {}
+        instance.dataset.label = "Test Label";
+        instance.queryResults["[data-id='label']"] = new ElementMock();
 
         await instance.connectedCallback();
         createMockChildren(instance);
@@ -24,7 +26,24 @@ describe("label-counter tests", () => {
         await instance.disconnectedCallback();
     })
 
-    it("initial", () => {
+    it("initial", async () => {
         assertEquals(instance.counter, 1);
+        assertEquals(instance.queryResults["[data-id='label']"].textContent, "Test Label");
+    })
+
+    it("update counter", async () => {
+        assertEquals(instance.counter, 1);
+
+        const btnIncrease = instance.querySelector('[data-id="increase"]');
+        const btnDecrease = instance.querySelector('[data-id="decrease"]');
+        const label = instance.querySelector("[data-id='value']");
+
+        await instance.clicked({ target: btnIncrease, stopPropagation: () => {} });
+        assertEquals(instance.counter, 2);
+        assertEquals(label.textContent, 2);
+
+        await instance.clicked({ target: btnDecrease, stopPropagation: () => {} });
+        assertEquals(instance.counter, 1);
+        assertEquals(label.textContent, 1);
     })
 })
