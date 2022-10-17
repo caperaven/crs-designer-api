880c5aeffb706f96216e16032f613e0b3fb4a81c
6Botter7 <7williamsandre7@gmail.com>
2022-10-17 12:55:25 +0200

    Designer-Repo
    Designer-repo component added

--- app/designer-repo-test/designer-repo-test.js
+++ app/designer-repo-test/designer-repo-test.js
@@ -1,8 +1,8 @@
 import "./../../components/designer-repo/designer-repo.js";
 
-export default class Welcome extends crsbinding.classes.ViewBase {
+export default class DesignerRepoTest extends crsbinding.classes.ViewBase {
     async connectedCallback() {
         await super.connectedCallback();
-        
     }
+
 }
---
+++ components/designer-repo/designer-repo.js
@@ -0,0 +1,55 @@
+export class DesignerRepo extends crsbinding.classes.BindableElement{
+    static get observedAttribute() {return ["data-repo"]};
+
+    get html() {
+        return import.meta.url.replace(".js", ".html");
+    }
+
+    async connectedCallback() {
+        await super.connectedCallback();
+        await crs.call("dom_interactive", "enable_resize", {
+            element: this,
+            resize_query : ".resize",
+            options: {}
+        })
+    }
+
+    async disconnectedCallback () {
+        await crs.call("dom_interactive", "disable_resize", {
+            element: this
+        })
+        await super.disconnectedCallback();
+    }
+
+    async #loadHTML () {
+        const status = this.getProperty("displayStatus");
+
+        if(status == null || this.dataset.repo == null) {
+            return ;
+        }
+        const file = `/templates/designer-repo/${this.dataset.repo}-${status}.html`;
+
+        this.container.innerHTML = await fetch(file).then(result => result.text());
+    }
+
+    preLoad () {
+        this.setProperty ("displayStatus", "grid");
+    }
+
+    async displayStatusChanged(newValue) {
+        await this.#loadHTML()
+    }
+
+    async attributeChangedCallback() {
+        await this.#loadHTML()
+    }
+
+    async filterChanged(newValue) {
+        await crs.call("dom_collection", "filter_children", {
+            element: this.container,
+            filter: newValue
+        })
+    }
+}
+
+customElements.define("designer-repo", DesignerRepo);
